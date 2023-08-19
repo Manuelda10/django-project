66e2f463a2255958ddfef6d131f068f3f89d5b8b
Manuel <dev@dev.com>
2023-08-18 21:56:40 -0500

    feat: Agregar conexión con SQL Server

--- pruebadjango/settings.py
+++ pruebadjango/settings.py
@@ -79,8 +79,16 @@ WSGI_APPLICATION = 'pruebadjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mssql',
+        'NAME': 'django_db',
+        'USER': 'sa',
+        'PASSWORD': 'Password1',
+        'HOST': 'localhost',
+        'PORT': '1433',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+            'Encrypt': 'Optional',
+        },
     }
 }
 
